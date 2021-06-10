950c277043fdea3fb353b15d124884acc0909152
cpereiramt <cpereiramt@gmail.com>
2021-06-10 11:53:35 -0300

    saving changes in project

---
+++ src/SimpleForm.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { axe } from 'jest-axe';
+
+import SimpleForm from './SimpleForm';
+
+describe('The <SimpleForm/> component', () => {
+  
+  it('should not fail any accessibility tests', async () => {
+    const { container } = render(<SimpleForm />);
+    expect(await axe(container)).toHaveNoViolations();
+  });
+  
+});
